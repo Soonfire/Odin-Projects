ce25fa27d6fc10c07338e96ad057e32beeb86bc8
Soonfire <sandi.sumandl@gmail.com>
2022-02-22 20:45:47 +0100

    some JS practice

---
+++ JSpractice/anotherifelse.js
@@ -0,0 +1,9 @@
+const pageQuestion = prompt("What is the 'official' name of JavaScript?");
+let response;
+if (pageQuestion === "ECMAScript") {
+    alert("Right!")
+}
+else {
+    alert("you don't know? ECMAScript!")
+}
+    
---
+++ JSpractice/numbersifelse.js
@@ -0,0 +1,11 @@
+const value = prompt("Write a numeric value")
+
+if (value > 0) {
+    alert("1")
+}
+else if (value < 0) {
+    alert("-1")
+}
+else {
+    alert("0")
+}
