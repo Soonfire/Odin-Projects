f771b8f363b49459a71d6c8ca1b909b365031dce
Soonfire <sandi.sumandl@gmail.com>
2022-03-07 18:58:57 +0100

    unfinished

--- eventHandlers/js/mouseTrail.js
+++ eventHandlers/js/mouseTrail.js
@@ -10,11 +10,29 @@ const createItem = () => {
     return div
 }
 
-function loadItems() {
-    const container = document.querySelector(".container");
-        for (let i = 0; i < 7; i++) {
-            container.appendChild(createItem());
-        }
+const container = document.querySelector(".container");
+    for (let i = 0; i < 7; i++) {
+    container.appendChild(createItem());
+    }
+const nodeList = document.getElementsByClassName("fpig")
+const nodeArray = [...nodeList]
+
+const followCursor = (e) => {
+    e.target.style.left = e.clientX + "px"
+    e.target.style.top = e.clientY + "px"
+    e.stopPropagation()
+    console.log(e.target)
 }
 
-window.addEventListener('load', loadItems)
+const joinSnake = () => {
+    for (const node of nodeArray) {
+        node.addEventListener('mousemove', followCursor);
+    }
+}
+
+window.addEventListener('load', joinSnake)
+
+/* pseudocode:
+    when mouseover the target, add mousemove to the target and move the target with cursor.
+    Every subsequent target added, should move according to previous target*/
+
